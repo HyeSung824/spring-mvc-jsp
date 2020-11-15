7d797424f1e445a07c5cb5b3138ac6b5bcefeb23
weblue3 <weblue2@gmail.com>
2020-11-15 19:36:20 +0900

    스프링 XML 설정방법 프로젝트 생성

---
+++ spring-xml-project/src/main/java/kr/co/softcampus/controller/HomeController.java
@@ -0,0 +1,25 @@
+package kr.co.softcampus.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(){
+        logger.info("home");
+        return "index";
+    }
+
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    public String test1(){
+        logger.info("test1");
+        return null;
+    }
+}
---
+++ spring-xml-project/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  작성자: 김홍준
+  작성일: 2020-11-15 오후 7:20
+--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hello Spring MVC</h1>
+    <img src="image/icon-spring-framework.svg"/>
+</body>
+</html>
