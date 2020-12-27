364fbbef9f5300058b123fc2f0c2391588ca9091
weblue3 <weblue2@gmail.com>
2020-12-28 07:29:09 +0900

    [스프링] 미니 프로젝트 mybatis 설정

--- spring-mini-project/src/main/java/kr/co/softcampus/config/ServletAppContext.java
+++ spring-mini-project/src/main/java/kr/co/softcampus/config/ServletAppContext.java
@@ -1,16 +1,39 @@
 package kr.co.softcampus.config;
 
+import kr.co.softcampus.mapper.BoardMapper;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("kr.co.softcampus.controller")
+@PropertySource(value = "/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
+
+    @Value("${db.classname}")
+    private String db_classname;
+
+    @Value("${db.url}")
+    private String db_url;
+
+    @Value("${db.username}")
+    private String db_username;
+
+    @Value("${db.password}")
+    private String db_password;
 
     //-- Controller의 메서드가 반환하는 jsp의 이름 앞뒤에 경로와 확장자를 붙혀주도록 설정한다.
     @Override
@@ -23,6 +46,31 @@ public class ServletAppContext implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
         registry.addResourceHandler("/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public BasicDataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(db_classname);
+        dataSource.setUrl(db_url);
+        dataSource.setUsername(db_username);
+        dataSource.setPassword(db_password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory sqlSessionFactory)throws Exception{
+        MapperFactoryBean<BoardMapper> mapperFactoryBean = new MapperFactoryBean<>(BoardMapper.class);
+        mapperFactoryBean.setSqlSessionFactory(sqlSessionFactory);
+        return mapperFactoryBean;
     }
 
 }
---
+++ spring-mini-project/src/main/java/kr/co/softcampus/mapper/BoardMapper.java
@@ -0,0 +1,9 @@
+package kr.co.softcampus.mapper;
+
+/**
+ * Created by 김홍준
+ * Date: 2020-12-28
+ * Time: 오전 7:13
+ */
+public interface BoardMapper {
+}
